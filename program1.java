f01f7dfda75c823408060c4283f8adf7240b4e2e
unnati sahu <unnatisahu911@gmail.com>
2025-07-01 09:23:15 +0530

    add java program for addition

---
+++ program1.java
@@ -0,0 +1,13 @@
+public class program1{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+System.ou.println("Enter first number");
+int a= sc.nextInt();
+
+System.ou.println("Enter second number");
+int b= sc.nextInt();
+
+int sum= a+b;
+System.out.println("Addition" + sum);
+}
+}
